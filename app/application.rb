4859f7c0d6da2022402840c6a08ed941c3627697
Matteo Muscas <thatman.j@gmail.com>
2019-03-07 10:53:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,11 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+      resp.write "#{item}\n"
+    end
+
     resp.finish
   end
 
